a34a76156f38b74bb6c43b2859d6def4237a455f
lhGWTS <mikan@LAPTOP-4NQ9CU1.localdomain>
2021-08-15 13:24:58 +0900

    my therd commit

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 from setuptools import setup, find_packages
-__version__ = "0.1.4"
 
 
 def readme():
@@ -9,30 +8,26 @@ def readme():
         return f.read()
 
 
-def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
+#def parse_requirements(filename):
+#    """ load requirements from a pip requirements file """
+#    lineiter = (line.strip() for line in open(filename))
+#    return [line for line in lineiter if line and not line.startswith("#")]
 
 
-install_requires = parse_requirements('requirements.txt')
+#install_requires = parse_requirements('requirements.txt')
 
 setup(
-    name="linus_colab_ssh",
-    version=__version__,
+    name="kkt_colab_ssh",
+    version="0.1",
     description='Create SSH tunel to a running colab notebook',
     long_description=readme(),
     long_description_content_type='text/markdown',
-    url='https://github.com/lamhoangtung/colab_ssh',
-    author='Hoang Tung Lam',
-    author_email='lamhoangtung.vz@gmail.com',
-    include_package_data=True,
+    url='https://github.com/lhGWTS/colab_ssh',
+    author='kkt',
+    author_email='kkt@gmail.com',
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    install_requires=install_requires,
     keywords=['ssh', 'colab'],
-    setup_requires=[],
-    dependency_links=[],
     python_requires='>=3',
     py_modules=['ssh_colab'],
     classifiers=[
@@ -42,4 +37,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
+    entry_points="""
+        [console_scripts]
+        colab_ssh = colab_ssh:colab_ssh:main
+        """
 )
